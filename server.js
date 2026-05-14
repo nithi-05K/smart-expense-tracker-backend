311229d932501568f900c2c90a04e5e1d5b2ac01
nithitha.k2022@vitstudent.ac.in <nithitha.k2022@vitstudent.ac.in>
2026-05-14 15:52:37 +0530

    Fixed render port issue

--- server.js
+++ server.js
@@ -24,7 +24,6 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
